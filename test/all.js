144723489c9e52fe58b6ab791e9c1c994d9d1523
tshemsedinov <timur.shemsedinov@gmail.com>
2018-04-01 16:08:25 +0300

    Log rotation
    Closes: https://github.com/metarhia/metalog/issues/7

--- test/all.js
+++ test/all.js
@@ -3,13 +3,74 @@
 const tap = require('tap');
 const metalog = require('..');
 
-tap.test('stub', (test) => {
-  const logger = metalog('./log', 'S1N1');
-  logger.on('open', () => {
-    logger.info('Test log message');
-    logger.close();
+const logger = metalog({
+  path: './log',
+  nodeId: 'S1N1',
+  writeInterval: 3000,
+  writeBuffer: 64 * 1024,
+  keepDays: 5
+});
+
+tap.test('logger.close before open', (test) => {
+  logger.close();
+  logger.on('close', () => {
+    test.notOk();
   });
+  test.end();
+});
+
+tap.test('logger.open', (test) => {
+  logger.on('open', () => {
+    test.end();
+  });
+  logger.on('error', (err) => {
+    test.error(err);
+    process.exit(1);
+  });
+});
+
+tap.test('logger.system', (test) => {
+  logger.system('Test log message');
+  test.end();
+});
+
+tap.test('logger.error', (test) => {
+  logger.error('Test log message');
+  test.end();
+});
+
+tap.test('logger.warn', (test) => {
+  logger.warn('Test log message');
+  test.end();
+});
+
+tap.test('logger.info', (test) => {
+  logger.info('Test log message');
+  test.end();
+});
+
+tap.test('logger.debug', (test) => {
+  logger.debug('Test log message');
+  test.end();
+});
+
+tap.test('logger.close', (test) => {
+  logger.removeAllListeners('close');
+  logger.close();
   logger.on('close', () => {
     test.end();
   });
+});
+
+tap.test('logger.close after close', (test) => {
+  logger.close();
+  logger.on('close', () => {
+    test.notOk();
+  });
+  test.end();
+});
+
+tap.test('logger.rotate', (test) => {
+  logger.rotate();
+  test.end();
 });
