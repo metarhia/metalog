1a33d56ae550567c59359c31dd43e8e81878cbe6
jedekar <ax.khodos@gmail.com>
2019-01-03 13:23:14 +0200

    Convert to class syntax
    Closes: https://github.com/metarhia/metalog/issues/49
    PR-URL: https://github.com/metarhia/metalog/pull/61

--- metalog.js
+++ metalog.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const fs = require('fs');
-const util = require('util');
 const events = require('events');
 const common = require('@metarhia/common');
 const concolor = require('concolor');
@@ -45,8 +44,13 @@ const logTypes = types => {
   return flags;
 };
 
-// Logger wrapper to bind it to certain application
+const normalizeStack = (stack) => stack.replace(/\s+at\s+/g, '\n\t');
 
+const lineStack = (stack) => stack.replace(/[\n\r]\s*/g, '; ');
+
+const pad = (s, len, char = ' ') => s + char.repeat(len - s.length);
+
+// Logger wrapper to bind it to certain application
 class ApplicationLogger {
 
   // logger <Logger>
@@ -61,12 +65,12 @@ class ApplicationLogger {
   }
 
   fatal(message) {
-    const msg = Logger.normalizeStack(message);
+    const msg = normalizeStack(message);
     this.logger.write('fatal', msg, this.application);
   }
 
   error(message) {
-    const msg = Logger.normalizeStack(message);
+    const msg = normalizeStack(message);
     this.logger.write('error', msg, this.application);
   }
 
@@ -79,7 +83,7 @@ class ApplicationLogger {
   }
 
   debug(message) {
-    const msg = Logger.normalizeStack(message);
+    const msg = normalizeStack(message);
     this.logger.write('debug', msg, this.application);
   }
 
@@ -93,165 +97,159 @@ class ApplicationLogger {
 
 }
 
-// Logger constructor
-//   path <string> log directory
-//   node <string> nodeId
-//   writeInterval <number> flush log to disk interval
-//   writeBuffer <number> buffer size 64kb
-//   keepDays <number> delete files after N days, 0 to disable
-//   toFile <string[]> write log types to file
-//   toStdout <string[]> write log types to stdout
-function Logger(options) {
-  const { path, node } = options;
-  const { writeInterval, writeBuffer, keepDays } = options;
-  const { toFile, toStdout } = options;
-  this.active = false;
-  this.path = path;
-  this.node = node;
-  this.writeInterval = writeInterval || 3000;
-  this.writeBuffer = writeBuffer || 64 * 1024;
-  this.keepDays = keepDays || 0;
-  this.options = { flags: 'a', highWaterMark: this.writeBuffer };
-  this.stream = null;
-  this.reopenTimer = null;
-  this.flushTimer = null;
-  this.lock = false;
-  this.buffer = [];
-  this.file = '';
-  this.toFile = logTypes(toFile);
-  this.toStdout = logTypes(toStdout);
-  this.open();
-}
+class Logger extends events.EventEmitter {
 
-util.inherits(Logger, events.EventEmitter);
-
-Logger.prototype.open = function() {
-  if (this.active) return;
-  this.active = true;
-  const date = common.nowDate();
-  this.file = this.path + '/' + date + '-' + this.node + '.log';
-  const now = new Date();
-  const nextDate = new Date();
-  nextDate.setUTCHours(0, 0, 0, 0);
-  const nextReopen = nextDate - now + DAY_MILLISECONDS;
-  this.reopenTimer = setTimeout(() => {
-    this.once('close', () => {
-      this.open();
-    });
-    this.close();
-  }, nextReopen);
-  if (this.keepDays) this.rotate();
-  this.stream = fs.createWriteStream(this.file, this.options);
-  this.flushTimer = setInterval(() => {
-    this.flush();
-  }, this.writeInterval);
-  this.stream.on('open', () => {
-    this.emit('open');
-  });
-  this.stream.on('error', () => {
-    this.emit('error', new Error('Can\'t open log file:' + this.file));
-  });
-};
-
-Logger.prototype.close = function() {
-  if (!this.active) return;
-  const stream = this.stream;
-  if (!stream || stream.destroyed || stream.closed) return;
-  this.flush((err) => {
-    if (err) return;
+  // path <string> log directory
+  // node <string> nodeId
+  // writeInterval <number> flush log to disk interval
+  // writeBuffer <number> buffer size 64kb
+  // keepDays <number> delete files after N days, 0 to disable
+  // toFile <string[]> write log types to file
+  // toStdout <string[]> write log types to stdout
+  constructor(options) {
+    super();
+    const { path, node } = options;
+    const { writeInterval, writeBuffer, keepDays } = options;
+    const { toFile, toStdout } = options;
     this.active = false;
-    this.stream.end(() => {
-      clearInterval(this.flushTimer);
-      clearTimeout(this.reopenTimer);
-      this.flushTimer = null;
-      this.reopenTimer = null;
-      const fileName = this.file;
-      this.emit('close');
-      fs.stat(fileName, (err, stats) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        if (stats.size > 0) return;
-        fs.unlink(this.file, (err) => {
-          console.log(err);
+    this.path = path;
+    this.node = node;
+    this.writeInterval = writeInterval || 3000;
+    this.writeBuffer = writeBuffer || 64 * 1024;
+    this.keepDays = keepDays || 0;
+    this.options = { flags: 'a', highWaterMark: this.writeBuffer };
+    this.stream = null;
+    this.reopenTimer = null;
+    this.flushTimer = null;
+    this.lock = false;
+    this.buffer = [];
+    this.file = '';
+    this.toFile = logTypes(toFile);
+    this.toStdout = logTypes(toStdout);
+    this.open();
+  }
+
+  open() {
+    if (this.active) return;
+    this.active = true;
+    const date = common.nowDate();
+    this.file = this.path + '/' + date + '-' + this.node + '.log';
+    const now = new Date();
+    const nextDate = new Date();
+    nextDate.setUTCHours(0, 0, 0, 0);
+    const nextReopen = nextDate - now + DAY_MILLISECONDS;
+    this.reopenTimer = setTimeout(() => {
+      this.once('close', () => {
+        this.open();
+      });
+      this.close();
+    }, nextReopen);
+    if (this.keepDays) this.rotate();
+    this.stream = fs.createWriteStream(this.file, this.options);
+    this.flushTimer = setInterval(() => {
+      this.flush();
+    }, this.writeInterval);
+    this.stream.on('open', () => {
+      this.emit('open');
+    });
+    this.stream.on('error', () => {
+      this.emit('error', new Error('Can\'t open log file:' + this.file));
+    });
+  }
+
+  close() {
+    if (!this.active) return;
+    const stream = this.stream;
+    if (!stream || stream.destroyed || stream.closed) return;
+    this.flush((err) => {
+      if (err) return;
+      this.active = false;
+      this.stream.end(() => {
+        clearInterval(this.flushTimer);
+        clearTimeout(this.reopenTimer);
+        this.flushTimer = null;
+        this.reopenTimer = null;
+        const fileName = this.file;
+        this.emit('close');
+        fs.stat(fileName, (err, stats) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          if (stats.size > 0) return;
+          fs.unlink(this.file, (err) => {
+            console.log(err);
+          });
         });
       });
     });
-  });
-};
+  }
 
-Logger.prototype.rotate = function() {
-  if (!this.keepDays) return;
-  fs.readdir(this.path, (err, files) => {
-    if (err) {
-      console.log(err);
+  rotate() {
+    if (!this.keepDays) return;
+    fs.readdir(this.path, (err, files) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const now = new Date();
+      const date = new Date(
+        now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0, 0
+      );
+      const time = date.getTime();
+      let i, fileName, fileTime, fileAge;
+      for (i in files) {
+        fileName = files[i];
+        fileTime = new Date(fileName.substring(0, 10)).getTime();
+        fileAge = Math.floor((time - fileTime) / DAY_MILLISECONDS);
+        if (fileAge > 1 && fileAge > this.keepDays - 1) {
+          fs.unlink(this.path + '/' + fileName, (err) => {
+            console.log(err);
+          });
+        }
+      }
+    });
+  }
+
+  write(type, message, application = 'default') {
+    const date = new Date();
+    if (this.toStdout[type]) {
+      const normalColor = textColor[type];
+      const markColor = typeColor[type];
+      const time = normalColor(date.toTimeString().substring(0, 8));
+      const mark = markColor(' ' + pad(type, 7));
+      const msg = normalColor(`${this.node}/${application}  ${message}`);
+      const line = `${time}  ${mark}  ${msg}`;
+      console.log(line);
+    }
+    if (this.toFile[type]) {
+      const time = date.toISOString();
+      const multiline = (/[\n\r]/g).test(message);
+      const line = multiline ? lineStack(message) : message;
+      const data = `${time} [${type}] ${this.node}/${application} ${line}\n`;
+      const buffer = Buffer.from(data);
+      this.buffer.push(buffer);
+    }
+  }
+
+  flush(callback) {
+    if (!this.active || this.lock || !this.buffer.length) {
+      if (callback) callback(new Error('Can\'t flush log buffer'));
       return;
     }
-    const now = new Date();
-    const date = new Date(
-      now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0, 0
-    );
-    const time = date.getTime();
-    let i, fileName, fileTime, fileAge;
-    for (i in files) {
-      fileName = files[i];
-      fileTime = new Date(fileName.substring(0, 10)).getTime();
-      fileAge = Math.floor((time - fileTime) / DAY_MILLISECONDS);
-      if (fileAge > 1 && fileAge > this.keepDays - 1) {
-        fs.unlink(this.path + '/' + fileName, (err) => {
-          console.log(err);
-        });
-      }
-    }
-  });
-};
-
-Logger.normalizeStack = (stack) => stack.replace(/\s+at\s+/g, '\n\t');
-
-Logger.lineStack = (stack) => stack.replace(/[\n\r]\s*/g, '; ');
-
-Logger.formatStack = (stack) => stack.replace(/; /g, '\n\t');
-
-const pad = (s, len, char = ' ') => s + char.repeat(len - s.length);
-
-Logger.prototype.write = function(type, message, application = 'default') {
-  const date = new Date();
-  if (this.toStdout[type]) {
-    const normalColor = textColor[type];
-    const markColor = typeColor[type];
-    const time = normalColor(date.toTimeString().substring(0, 8));
-    const mark = markColor(' ' + pad(type, 7));
-    const msg = normalColor(`${this.node}/${application}  ${message}`);
-    const line = `${time}  ${mark}  ${msg}`;
-    console.log(line);
+    this.lock = true;
+    const buffer = Buffer.concat(this.buffer);
+    this.buffer.length = 0;
+    this.stream.write(buffer, (err) => {
+      this.lock = false;
+      if (callback) callback(err);
+    });
   }
-  if (this.toFile[type]) {
-    const time = date.toISOString();
-    const multiline = (/[\n\r]/g).test(message);
-    const line = multiline ? Logger.lineStack(message) : message;
-    const data = `${time} [${type}] ${this.node}/${application} ${line}\n`;
-    const buffer = Buffer.from(data);
-    this.buffer.push(buffer);
-  }
-};
 
-Logger.prototype.flush = function(callback) {
-  if (!this.active || this.lock || !this.buffer.length) {
-    if (callback) callback(new Error('Can\'t flush log buffer'));
-    return;
+  bind(application) {
+    return new ApplicationLogger(this, application);
   }
-  this.lock = true;
-  const buffer = Buffer.concat(this.buffer);
-  this.buffer.length = 0;
-  this.stream.write(buffer, (err) => {
-    this.lock = false;
-    if (callback) callback(err);
-  });
-};
 
-Logger.prototype.bind = function(application) {
-  return new ApplicationLogger(this, application);
-};
+}
 
 module.exports = (args) => new Logger(args);
