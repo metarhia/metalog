8ba27c11031d9751c77465974936b4f91103e278
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-23 06:21:35 +0300

    fixup! Add tests for open/close promises

--- test/file.js
+++ test/file.js
@@ -60,66 +60,41 @@ const createLogger = () =>
     logger1.close();
   }, 500);
 
-  const logger2 = await createLogger();
-
-  metatests.test('logger write more then 60Mb', test => {
-    logger2.toStdout.INFO = false;
+  metatests.test('logger write more then 60Mb', async test => {
+    const logger = await createLogger();
+    logger.toStdout.INFO = false;
     const begin = process.hrtime();
     for (let i = 0; i < 1000000; i++) {
-      logger2.info('Write more then 60Mb logs, line: ' + i);
+      logger.info('Write more then 60Mb logs, line: ' + i);
     }
-    logger2.close();
-    logger2.on('close', () => {
+    logger.on('close', () => {
       const end = process.hrtime(begin);
       const time = end[0] * 1e9 + end[1];
       console.log({ time });
       test.end();
     });
+    await logger.close();
   });
 
-  const logger3 = await createLogger();
-
-  metatests.test('logger.close', test => {
-    logger3.info('Info log message');
-    logger3.on('close', () => {
-      test.end();
-    });
-    logger3.close();
-  });
-
-  const logger4 = await createLogger();
-
-  metatests.test('logger.close after close', test => {
-    logger4.info('Info log message');
-    logger4.close();
-    logger4.on('close', () => {
-      logger4.removeAllListeners('close');
-      logger4.on('close', test.mustNotCall());
-      logger4.close();
-      test.end();
-    });
-  });
-
-  const logger5 = await createLogger();
-
-  metatests.test('logger.rotate', test => {
-    logger5.rotate();
-    logger5.close();
+  metatests.test('logger.close', async test => {
+    const logger = await createLogger();
+    logger.info('Info log message');
+    await logger.close();
     test.end();
   });
 
-  /*metatests.test('await metalog(options)', test => {
-    createLogger().then(logger => {
-      test.end();
-      logger.close();
-    });
-  });*/
+  metatests.test('logger.close after close', async test => {
+    const logger = await createLogger();
+    logger.info('Info log message');
+    await logger.close();
+    await logger.close();
+    test.end();
+  });
 
-  /*metatests.test('await logger.close', test => {
-    createLogger().then(logger => {
-      logger.close().then(() => {
-        test.end();
-      });
-    });
-  });*/
+  metatests.test('logger.rotate', async test => {
+    const logger = await createLogger();
+    logger.rotate();
+    await logger.close();
+    test.end();
+  });
 })();
