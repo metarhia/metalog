37b7d4418af788c0c27029b04681954a09d43cbf
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-05-23 03:26:11 +0300

    Add tests for open/close promises
    Refs: https://github.com/metarhia/metalog/issues/117
    PR-URL: https://github.com/metarhia/metalog/pull/118

--- test/display.js
+++ test/display.js
@@ -2,24 +2,26 @@
 
 const metalog = require('..');
 
-const logger = metalog({
-  path: './log',
-  workerId: 7,
-  writeInterval: 3000,
-  writeBuffer: 64 * 1024,
-  keepDays: 5,
-});
+(async () => {
+  const logger = await metalog({
+    path: './log',
+    workerId: 7,
+    writeInterval: 3000,
+    writeBuffer: 64 * 1024,
+    keepDays: 5,
+  });
 
-const stack = new Error('Stack test log message').stack;
-logger.debug(stack);
-logger.system('System test log message');
-logger.fatal('Fatal test log message');
-logger.error('Error test log message');
-logger.warn('Warning test log message');
-logger.info('Info test log message');
-logger.debug('Debug test log message');
-logger.access('Access test log message');
-logger.slow('Slow test log message');
-logger.db('Database test log message');
+  const stack = new Error('Stack test log message').stack;
+  logger.debug(stack);
+  logger.system('System test log message');
+  logger.fatal('Fatal test log message');
+  logger.error('Error test log message');
+  logger.warn('Warning test log message');
+  logger.info('Info test log message');
+  logger.debug('Debug test log message');
+  logger.access('Access test log message');
+  logger.slow('Slow test log message');
+  logger.db('Database test log message');
 
-logger.close();
+  logger.close();
+})();
--- test/file.js
+++ test/file.js
@@ -13,112 +13,88 @@ const createLogger = () =>
     toStdout: [],
   });
 
-const logger1 = createLogger();
+(async () => {
+  const logger1 = await createLogger();
 
-metatests.test('logger.open', test => {
-  logger1.on('open', () => {
+  metatests.test('logger.system', test => {
+    logger1.system('System test log message');
     test.end();
   });
-  logger1.on('error', err => {
-    test.error(err);
-    process.exit(1);
-  });
-  logger1.open();
-  logger1.close();
-});
 
-metatests.test('logger.system', test => {
-  logger1.system('System test log message');
-  test.end();
-});
-
-metatests.test('logger.fatal', test => {
-  logger1.fatal('Fatal test log message');
-  test.end();
-});
-
-metatests.test('logger.error', test => {
-  logger1.error('Error test log message');
-  test.end();
-});
-
-metatests.test('logger.warn', test => {
-  logger1.warn('Warning test log message');
-  test.end();
-});
-
-metatests.test('logger.info', test => {
-  logger1.info('Info test log message');
-  test.end();
-});
-
-metatests.test('logger.debug', test => {
-  logger1.debug('Debug test log message');
-  test.end();
-});
-
-metatests.test('logger.slow', test => {
-  logger1.slow('Slow test log message');
-  test.end();
-});
-
-metatests.test('logger.db', test => {
-  logger1.db('Database test log message');
-  test.end();
-});
-
-setTimeout(() => {
-  logger1.close();
-}, 500);
-
-const logger2 = createLogger();
-
-metatests.test('logger write more then 60Mb', test => {
-  logger2.open();
-  logger2.toStdout.INFO = false;
-  const begin = process.hrtime();
-  for (let i = 0; i < 1000000; i++) {
-    logger2.info('Write more then 60Mb logs, line: ' + i);
-  }
-  logger2.close();
-  logger2.on('close', () => {
-    const end = process.hrtime(begin);
-    const time = end[0] * 1e9 + end[1];
-    console.log({ time });
-    logger2.close();
+  metatests.test('logger.fatal', test => {
+    logger1.fatal('Fatal test log message');
     test.end();
   });
-});
 
-const logger3 = createLogger();
-
-metatests.test('logger.close', test => {
-  logger3.open();
-  logger3.info('Info log message');
-  logger3.on('close', () => {
+  metatests.test('logger.error', test => {
+    logger1.error('Error test log message');
     test.end();
   });
-  logger3.close();
-});
 
-const logger4 = createLogger();
-
-metatests.test('logger.close after close', test => {
-  logger4.open();
-  logger4.info('Info log message');
-  logger4.close();
-  logger4.on('close', () => {
-    logger4.removeAllListeners('close');
-    logger4.on('close', test.mustNotCall());
-    logger4.close();
+  metatests.test('logger.warn', test => {
+    logger1.warn('Warning test log message');
     test.end();
   });
-});
 
-const logger5 = createLogger();
+  metatests.test('logger.info', test => {
+    logger1.info('Info test log message');
+    test.end();
+  });
 
-metatests.test('logger.rotate', test => {
-  logger5.rotate();
-  logger5.close();
-  test.end();
-});
+  metatests.test('logger.debug', test => {
+    logger1.debug('Debug test log message');
+    test.end();
+  });
+
+  metatests.test('logger.slow', test => {
+    logger1.slow('Slow test log message');
+    test.end();
+  });
+
+  metatests.test('logger.db', test => {
+    logger1.db('Database test log message');
+    test.end();
+  });
+
+  setTimeout(() => {
+    logger1.close();
+  }, 500);
+
+  metatests.test('logger write more then 60Mb', async test => {
+    const logger = await createLogger();
+    logger.toStdout.INFO = false;
+    const begin = process.hrtime();
+    for (let i = 0; i < 1000000; i++) {
+      logger.info('Write more then 60Mb logs, line: ' + i);
+    }
+    logger.on('close', () => {
+      const end = process.hrtime(begin);
+      const time = end[0] * 1e9 + end[1];
+      console.log({ time });
+      test.end();
+    });
+    await logger.close();
+  });
+
+  metatests.test('logger.close', async test => {
+    const logger = await createLogger();
+    logger.info('Info log message');
+    await logger.close();
+    test.end();
+  });
+
+  metatests.test('logger.close after close', async test => {
+    const logger = await createLogger();
+    logger.info('Info log message');
+    await logger.close();
+    await logger.close();
+    test.end();
+  });
+
+  metatests.test('logger.rotate', async test => {
+    const logger = await createLogger();
+    logger.rotate();
+    await logger.close();
+    test.end();
+  });
+})();
