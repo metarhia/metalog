0d1d765c29cb8ca9c9d87d72ae6694ae22c7cca1
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-06-23 02:24:38 +0300

    Truncate paths in stack traces with options: { home }

--- test/display.js
+++ test/display.js
@@ -9,6 +9,7 @@ const metalog = require('..');
     writeInterval: 3000,
     writeBuffer: 64 * 1024,
     keepDays: 5,
+    home: process.cwd(),
   });
 
   const stack = new Error('Stack test log message').stack;
