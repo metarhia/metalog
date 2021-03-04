0544c7fde222b3d03f663dfd31cfbac21f9eb51a
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-03-04 12:44:25 +0200

    Add typings

---
+++ types/metalog.d.ts
@@ -0,0 +1,44 @@
+import EventEmitter = require('events');
+
+interface LoggerOptions {
+  path: string;
+  home: string;
+  workerId: number;
+  createStream: () => NodeJS.WritableStream;
+  writeInterval: number;
+  writeBuffer: number;
+  keepDays: number;
+  toFile: Array<string>;
+  toStdout: Array<string>;
+}
+
+export class Logger extends EventEmitter {
+  active: boolean;
+  path: string;
+  workerId: string;
+  createStream: () => NodeJS.WritableStream;
+  writeInterval: number;
+  writeBuffer: number;
+  keepDays: number;
+  home: string;
+  stream: NodeJS.WritableStream;
+  reopenTimer: NodeJS.Timer;
+  flushTimer: NodeJS.Timer;
+  lock: boolean;
+  buffer: Array<Buffer>;
+  file: string;
+  toFile: Array<string>;
+  fsEnabled: boolean;
+  toStdout: Array<string>;
+  console: Console;
+  constructor(args: LoggerOptions);
+  createLogDir(): Promise<void>;
+  open(): Promise<Logger>;
+  close(): Promise<void>;
+  rotate(): Promise<void>;
+  write(type: string, s: string): void;
+  flush(callback: Function): void;
+  normalizeStack(stack: string): string;
+}
+
+export function openLog(args: LoggerOptions): Promise<Logger>;
